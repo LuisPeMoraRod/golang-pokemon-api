4cc92a4f536c1e63c3b0fc5e96028b5b742bffa9
Luis Pedro Morales Rodriguez <luismorarod98@gmail.com>
2020-10-21 19:38:53 -0600

    Add language and script in .travis.yml

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"pokemon-api/database"
@@ -13,12 +14,37 @@ func getAllPokemons(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(database.PokemonDb)
 }
 
+func getPokemon(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range database.PokemonDb {
+		if article.ID == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func addPokemon(w http.ResponseWriter, r *http.Request){
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var pokemon database.Pokemon
+	json.Unmarshal(reqBody, &pokemon)
+	database.PokemonDb = append(database.PokemonDb, pokemon)
+	json.NewEncoder(w).Encode(pokemon)
+}
+
+//func createPokemon()
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
+	myRouter.HandleFunc("/pokemons/new", addPokemon).Methods("POST")
+	myRouter.HandleFunc("/pokemons/{id}", getPokemon).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
+
 
 func commonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
